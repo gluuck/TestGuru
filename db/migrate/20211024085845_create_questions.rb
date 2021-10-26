51c4670d1a05a4629566665925ba84122080f2c9
Vladimir Ostapenko <ostapenkov74@gmail.com>
2021-10-26 15:06:46 +0500

    bags

--- db/migrate/20211024085845_create_questions.rb
+++ db/migrate/20211024085845_create_questions.rb
@@ -2,8 +2,8 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.string :body, null: false
-      t.belongs_to :test, foreign_key: true
-      
+      t.belongs_to :test, null: false, foreign_key: true
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_041932) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
-    t.integer "question_id"
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_041932) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
@@ -38,21 +38,23 @@ ActiveRecord::Schema.define(version: 2021_10_25_041932) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
-    t.integer "author_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "autrhor_id", null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "authors"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
