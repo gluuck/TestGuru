16358845a9003bacc7e04d20e88fd2b028f3381f
Vladimir Ostapeno <ostapenkov74@gmail.com>
2021-10-28 11:47:15 +0500

    change Test method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 
 	def self.test_titles(title)
-		Test.order(title: :desc).where(title: Category.find_by(title: title).title)
+		Test.order(title: :desc).where(title: Category.find_by(title: title).title).pluck(:title)
 	end
 end
