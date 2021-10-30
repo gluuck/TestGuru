19df8233610e1e1eb0785b24524715c8590f346c
Vladimir Ostapenko <ostapenkov74@gmail.com>
2021-10-30 15:35:01 +0500

    change user request

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
-class User < ApplicationRecord
-	has_many :tests, through: :results
+# frozen_string_literal: true
 
-	def find_by_level(level)
-		Test.joins('JOIN users ON users.id = tests.user_id').where(tests: {level: level}).pluck(:title)
-	end
+class User < ApplicationRecord
+  has_many :tests, through: :results
+
+  def find_by_level(level)
+    Test.joins('JOIN results ON results.test_id = tests.id').where(tests: { level: level }).pluck(:title)
+  end
 end
