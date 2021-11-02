30162092ff6552e3f5f99aa6f4ac8568510f0e31
Vladimir Ostapenko <ostapenkov74@gmail.com>
2021-11-02 16:14:03 +0500

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+  
+  has_many :questions, dependent: :destroy
+  has_many :results
+  has_many :users, through: :results
 
   def self.test_titles(title)
     joins('JOIN categories ON categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests , through: :results
 
   def find_by_level(level)
     Test.joins('JOIN results ON results.test_id = tests.id')
