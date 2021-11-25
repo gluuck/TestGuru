2cdc14b1c2484a13d84bc19e63cd8511007fe32c
Gluuck <ostapenkov74@gmail.com>
2021-11-25 04:03:27 -0800

    add translations to locale

--- Gemfile
+++ Gemfile
@@ -26,14 +26,13 @@ gem 'jbuilder', '~> 2.7'
 gem 'devise', '~> 4.2'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'rails-i18n'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
-  gem 'faker'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_user_permit, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     resource.is_a?(Admin) ? admin_tests_path : root_path
   end
 
+  def default_url_options
+    I18n.default_locale == I18n.locale ? {} : {lang: I18n.locale}
+  end
+  
+
   private
 
   def set_user_permit
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-  end  
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:locale] : I18n.default_locale
+  end
+  
 end
